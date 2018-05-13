df97bab30f8473920f03e23dc9b7d2e847896cd6
ReKT <mineblock20@gmail.com>
2018-05-14 07:25:50 +1000

    using host to total all instances

--- modules/discordbots.py
+++ modules/discordbots.py
@@ -8,6 +8,10 @@ connection = pymysql.connect(host="localhost",
                                      password="rektdiscord",
                                      db="nekobot",
                                      port=3306)
+stats2 = ["OwO whats n!help", "🤔🤔🤔", "👀", "(╯°□°）╯︵ ┻━┻",
+                  "¯\_(ツ)_/¯", "┬─┬ノ(ಠ_ಠノ)", "><(((('>", "_/\__/\__0>", "ô¿ô", "°º¤ø,¸¸,ø¤º°`°º¤ø,", "=^..^=",
+                  "龴ↀ◡ↀ龴", "^⨀ᴥ⨀^", "^⨀ᴥ⨀^", "⨌⨀_⨀⨌", "•|龴◡龴|•", "ˁ˚ᴥ˚ˀ", "⦿⽘⦿", " (╯︵╰,)",
+                  " (╯_╰)", "㋡", "ˁ˚ᴥ˚ˀ", "\(^-^)/"]
 db = connection.cursor()
 
 class DiscordBotsOrgAPI:
@@ -16,21 +20,28 @@ class DiscordBotsOrgAPI:
     def __init__(self, bot):
         self.bot = bot
         self.token = config.dbots_key
-        self.dblpy = dbl.Client(self.bot, self.token)
 
     async def startdbl(self):
-        stats2 = [f"Servers: {len(self.bot.guilds)}", f"Users: {len(set(self.bot.get_all_members()))}",
-                  "OwO whats n!help", "🤔🤔🤔", f"{self.bot.shard_count} Shards OwO", "👀", "(╯°□°）╯︵ ┻━┻",
-                  "¯\_(ツ)_/¯", "┬─┬ノ(ಠ_ಠノ)", "><(((('>", "_/\__/\__0>", "ô¿ô", "°º¤ø,¸¸,ø¤º°`°º¤ø,", "=^..^=",
-                  "龴ↀ◡ↀ龴", "^⨀ᴥ⨀^", "^⨀ᴥ⨀^", "⨌⨀_⨀⨌", "•|龴◡龴|•", "ˁ˚ᴥ˚ˀ", "⦿⽘⦿", " (╯︵╰,)",
-                  " (╯_╰)", "㋡", "ˁ˚ᴥ˚ˀ", "\(^-^)/"]
         while True:
+            print("Getting all servers.")
+            async with aiohttp.ClientSession() as cs:
+                async with cs.get("http://localhost:1212") as r:
+                    total = await r.json()
+            totalservers = total["count"]
             print("Attempting to update server count.")
-            db.execute(f"INSERT INTO guildcount VALUES ({len(self.bot.guilds)}, {int(time.time())})")
+            db.execute(f"INSERT INTO guildcount VALUES ({totalservers}, {int(time.time())})")
             connection.commit()
             try:
                 await self.dblpy.post_server_count(shard_count=self.bot.shard_count, shard_no=self.bot.shard_id)
-                print("Posted server count. {}".format(len(self.bot.guilds)))
+                url = "https://discordbots.org/api/bots/310039170792030211/stats"
+                payload = {
+                    "server_count": int(totalservers)
+                }
+                async with aiohttp.ClientSession() as cs:
+                    async with cs.post(url, json=payload, headers={"Authorization": config.dbots_key}) as r:
+                        res = await r.json()
+                print(res)
+                print("Posted server count. {}".format(totalservers))
                 game = discord.Streaming(name=random.choice(stats2), url="https://www.twitch.tv/rektdevlol")
                 await self.bot.change_presence(activity=game)
             except Exception as e:
@@ -40,7 +51,7 @@ class DiscordBotsOrgAPI:
                 async with aiohttp.ClientSession() as session:
                     async with session.post('https://bots.discord.pw/api/bots/310039170792030211/stats',
                                             headers={'Authorization': f'{config.dpw_key}'},
-                                            json={"server_count": len(self.bot.guilds),
+                                            json={"server_count": int(totalservers),
                                                   "shard_count": self.bot.shard_count}) as response:
                         t = await response.read()
                         print(t)
@@ -51,7 +62,7 @@ class DiscordBotsOrgAPI:
                 async with aiohttp.ClientSession() as session:
                     async with session.post('https://ls.terminal.ink/api/v1/bots/310039170792030211',
                                             headers={'Authorization': f'{config.terminal_key}'},
-                                            data={"server_count": len(self.bot.guilds)}) as response:
+                                            data={"server_count": int(totalservers)}) as response:
                         t = await response.json()
                         print(t)
             except Exception as e:
