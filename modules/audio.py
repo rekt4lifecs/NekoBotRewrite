ca6b0dcb7cc60ec4925e751caec71125c5cdaff3
ReKT <mineblock20@gmail.com>
2018-06-04 07:23:08 +1000

    update audio + add char

--- modules/audio.py
+++ modules/audio.py
@@ -33,7 +33,7 @@ class Audio:
         if not hasattr(bot, 'lavalink'):
             lavalink.Client(bot=bot,
                             host=config.lavalink['host'],
-                            ws_port=8080,
+                            ws_port=3232,
                             password=config.lavalink['password'],
                             loop=self.bot.loop, log_level=loginfo)
             self.bot.lavalink.register_hook(self.track_hook)
