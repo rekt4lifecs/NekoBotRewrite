9ee7a4e562b2af01aa2476867b6a50cbfa7a41dc
hibiki <rekt@nekobot.xyz>
2018-06-12 20:00:18 +1000

    megaheccinbiglogger

--- bot.py
+++ bot.py
@@ -6,12 +6,70 @@ import aiohttp, aioredis, aiomysql
 import os
 import config
 
-log = logging.getLogger()
-log.setLevel(logging.INFO)
-logFormatter = logging.Formatter("%(asctime)s [%(threadName)-12.12s] [%(levelname)-5.5s]  %(message)s")
-consoleHandler = logging.StreamHandler()
-consoleHandler.setFormatter(logFormatter)
-log.addHandler(consoleHandler)
+BLACK, RED, GREEN, YELLOW, BLUE, MAGENTA, CYAN, WHITE = range(8)
+RESET_SEQ = "\033[0m"
+COLOR_SEQ = "\033[1;%dm"
+BOLD_SEQ = "\033[1m"
+TIME_SEQ = COLOR_SEQ % (30 + MAGENTA)
+NAME_SEQ = COLOR_SEQ % (30 + CYAN)
+FORMAT = "[$TIME_SEQ%(asctime)-3s$RESET]" \
+         "[$NAME_SEQ$BOLD%(name)-2s$RESET]" \
+         "[%(levelname)-1s]" \
+         "[%(message)s]" \
+         "[($BOLD%(filename)s$RESET:%(lineno)d)]"
+
+
+def formatter_message(message: str, colored: bool = True):
+    if colored:
+        message = message.replace("$RESET", RESET_SEQ)
+        message = message.replace("$BOLD", BOLD_SEQ)
+        message = message.replace("$TIME_SEQ", TIME_SEQ)
+        message = message.replace("$NAME_SEQ", NAME_SEQ)
+        return message
+    else:
+        message = message.replace("$RESET", "")
+        message = message.replace("$BOLD", "")
+        return message
+
+class ColoredFormatter(logging.Formatter):
+    def __init__(self, msg, use_color=True):
+        logging.Formatter.__init__(self, msg)
+        self.use_color = use_color
+
+    def format(self, record):
+        level_name = record.levelname
+        if self.use_color and level_name in COLORS:
+            level_name_color = COLOR_SEQ % (30 + COLORS[level_name]) + level_name + RESET_SEQ
+            record.levelname = level_name_color
+        message = record.msg
+        if self.use_color and level_name in COLORS:
+            message_color = COLOR_SEQ % (30 + BLUE) + message + RESET_SEQ
+            record.msg = message_color
+        return logging.Formatter.format(self, record)
+
+class ColoredLogger(logging.Logger):
+    def __init__(self, name):
+        logging.Logger.__init__(self, name, logging.INFO)
+        return
+
+COLORS = {
+    'WARNING': YELLOW,
+    'INFO': BLUE,
+    'DEBUG': WHITE,
+    'CRITICAL': YELLOW,
+    'ERROR': RED
+}
+
+logger = logging.getLogger()
+color_format = formatter_message(FORMAT, True)
+logging.setLoggerClass(ColoredLogger)
+color_formatter = ColoredFormatter(color_format)
+console = logging.StreamHandler()
+file = logging.FileHandler(filename=f'logs/nko.log', encoding='utf-8', mode='w')
+console.setFormatter(color_formatter)
+file.setFormatter(color_formatter)
+logger.addHandler(console)
+logger.addHandler(file)
 
 class bcolors:
     HEADER = '\033[95m'
@@ -90,7 +148,6 @@ class NekoBot(commands.AutoShardedBot):
         await self.process_commands(message)
 
     async def close(self):
-        print(bcolors.FAIL + "[CLOSING]" + bcolors.ENDC)
         await super().close()
         await self.close()
 
