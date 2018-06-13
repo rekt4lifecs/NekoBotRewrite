5266ea786aa30dabd921f775b3cd35e6c2612909
hibiki <rekt@nekobot.xyz>
2018-06-14 02:01:32 +1000

    async sql :^)

--- modules/discordbots.py
+++ modules/discordbots.py
@@ -1,20 +1,13 @@
 import asyncio, config, discord, random, aiohttp
-import pymysql, time
 import logging
 
 log = logging.getLogger()
 
 messages = ["OwO Whats this", "MonkaS", "OwO", "Haiiiii", ".help", "🤔🤔🤔", "HMMM🤔", "USE n! WEW", "n!HELP REE"]
-connection = pymysql.connect(host="localhost",
-                                     user="root",
-                                     password="rektdiscord",
-                                     db="nekobot",
-                                     port=3306)
 stats2 = ["OwO whats n!help", "🤔🤔🤔", "👀", "(╯°□°）╯︵ ┻━┻",
                   "¯\_(ツ)_/¯", "┬─┬ノ(ಠ_ಠノ)", "><(((('>", "_/\__/\__0>", "ô¿ô", "°º¤ø,¸¸,ø¤º°`°º¤ø,", "=^..^=",
                   "龴ↀ◡ↀ龴", "^⨀ᴥ⨀^", "^⨀ᴥ⨀^", "⨌⨀_⨀⨌", "•|龴◡龴|•", "ˁ˚ᴥ˚ˀ", "⦿⽘⦿", " (╯︵╰,)",
                   " (╯_╰)", "㋡", "ˁ˚ᴥ˚ˀ", "\(^-^)/"]
-db = connection.cursor()
 
 class DiscordBotsOrgAPI:
     """Handles interactions with the discordbots.org API"""
@@ -23,23 +16,34 @@ class DiscordBotsOrgAPI:
         self.bot = bot
         self.token = config.dbots_key
 
+    async def execute(self, query: str, isSelect: bool = False, fetchAll: bool = False, commit: bool = False):
+        async with self.bot.sql_conn.acquire() as conn:
+            async with conn.cursor() as db:
+                await db.execute(query)
+                if isSelect:
+                    if fetchAll:
+                        values = await db.fetchall()
+                    else:
+                        values = await db.fetchone()
+                if commit:
+                    await conn.commit()
+            if isSelect:
+                return values
+
     async def startdbl(self):
         while True:
             log.info("Getting all servers.")
-            totalservers = len(self.bot.guilds)
             log.info("Attempting to update server count.")
-            db.execute(f"INSERT INTO guildcount VALUES ({totalservers}, {int(time.time())})")
-            connection.commit()
+            servers = await self.execute("SELECT sum(guilds) FROM instances", isSelect=True)
             try:
                 url = "https://discordbots.org/api/bots/310039170792030211/stats"
                 payload = {
-                    "server_count": int(totalservers),
-                    "shard_id": self.bot.shard_id,
+                    "server_count": int(servers),
                     "shard_count": len(self.bot.shards)
                 }
                 async with aiohttp.ClientSession() as cs:
                     await cs.post(url, json=payload, headers={"Authorization": config.dbots_key})
-                log.info("Posted server count. {}".format(totalservers))
+                log.info("Posted server count. {}".format(servers))
                 game = discord.Streaming(name=random.choice(stats2), url="https://www.twitch.tv/rektdevlol")
                 await self.bot.change_presence(activity=game)
             except Exception as e:
@@ -49,7 +53,7 @@ class DiscordBotsOrgAPI:
                 async with aiohttp.ClientSession() as session:
                     await session.post('https://bots.discord.pw/api/bots/310039170792030211/stats',
                                             headers={'Authorization': f'{config.dpw_key}'},
-                                            json={"server_count": int(totalservers),
+                                            json={"server_count": int(servers),
                                                   "shard_count": self.bot.shard_count})
             except Exception as e:
                 log.warning(f"Failed to post to pw, {e}")
