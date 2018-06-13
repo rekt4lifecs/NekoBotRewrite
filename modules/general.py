e52c2b2daff0caa346fce2ec958bd9fbb10ddceb
hibiki <rekt@nekobot.xyz>
2018-06-14 02:01:21 +1000

    is now getting guildsfromdb

--- modules/general.py
+++ modules/general.py
@@ -200,7 +200,7 @@ class General:
     async def info(self, ctx):
         """Get Bot's Info"""
         await ctx.trigger_typing()
-        servers = len(self.bot.guilds)
+        servers = await self.execute("SELECT sum(guilds) FROM instances", isSelect=True)
         lang = await self.bot.redis.get(f"{ctx.message.author.id}-lang")
         if lang:
             lang = lang.decode('utf8')
