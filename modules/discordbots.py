5afe101293313f0c5fb3d0eb973a2f5012424fda
hibiki <rekt@nekobot.xyz>
2018-06-16 18:02:40 +1000

    wew

--- modules/discordbots.py
+++ modules/discordbots.py
@@ -35,34 +35,33 @@ class DiscordBotsOrgAPI:
             log.info("Getting all servers.")
             log.info("Attempting to update server count.")
             servers = (await self.execute("SELECT sum(guilds) FROM instances", isSelect=True))[0]
-            try:
-                url = "https://discordbots.org/api/bots/310039170792030211/stats"
-                payload = {
-                    "server_count": int(servers),
-                    "shard_count": self.bot.shard_count
-                }
-                async with aiohttp.ClientSession() as cs:
-                    await cs.post(url, json=payload, headers={"Authorization": config.dbots_key})
-                log.info("Posted server count. {}".format(servers))
-                game = discord.Streaming(name=random.choice(stats2), url="https://www.twitch.tv/rektdevlol")
-                await self.bot.change_presence(activity=game)
-            except Exception as e:
-                log.warning('Failed to post server count\n{}: {}'.format(type(e).__name__, e))
+            game = discord.Streaming(name=random.choice(stats2), url="https://www.twitch.tv/rektdevlol")
+            await self.bot.change_presence(activity=game)
+            if self.bot.instance == 0:
+                try:
+                    url = "https://discordbots.org/api/bots/310039170792030211/stats"
+                    payload = {
+                        "server_count": int(servers),
+                        "shard_count": self.bot.shard_count
+                    }
+                    async with aiohttp.ClientSession() as cs:
+                        await cs.post(url, json=payload, headers={"Authorization": config.dbots_key})
+                    log.info("Posted server count. {}".format(servers))
+                except Exception as e:
+                    log.warning('Failed to post server count\n{}: {}'.format(type(e).__name__, e))
 
-            try:
-                async with aiohttp.ClientSession() as session:
-                    await session.post('https://bots.discord.pw/api/bots/310039170792030211/stats',
-                                            headers={'Authorization': f'{config.dpw_key}'},
-                                            json={"server_count": int(servers),
-                                                  "shard_count": self.bot.shard_count})
-            except Exception as e:
-                log.warning(f"Failed to post to pw, {e}")
+                try:
+                    async with aiohttp.ClientSession() as session:
+                        await session.post('https://bots.discord.pw/api/bots/310039170792030211/stats',
+                                                headers={'Authorization': f'{config.dpw_key}'},
+                                                json={"server_count": int(servers),
+                                                      "shard_count": self.bot.shard_count})
+                except Exception as e:
+                    log.warning(f"Failed to post to pw, {e}")
             await asyncio.sleep(1800)
 
 
     async def on_ready(self):
-        if self.bot.instance != 0:
-            return
         await self.startdbl()
 
 def setup(bot):
