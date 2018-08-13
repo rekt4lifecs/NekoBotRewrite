eee4b7d51c1229ae5ccbb026e4d8e2ec11d60811
ReKT <20015683+ReKTDevlol@users.noreply.github.com>
2018-08-13 10:47:13 +1000

    move imgwelcoming to rethink

--- modules/imgwelcome.py
+++ modules/imgwelcome.py
@@ -3,50 +3,52 @@ from PIL import Image, ImageDraw, ImageFont, ImageOps
 import discord, os, aiohttp, string
 from io import BytesIO
 import textwrap
+import rethinkdb as r
+import rethinkdb as rethonk
+import base64
 
 class IMGWelcome:
 
     def __init__(self, bot):
         self.bot = bot
-        self.cached_guilds = []
+
+    async def __is_enabled(self, guild:int):
+        if await r.table("imgwelcome").get(str(guild)).run(self.bot.r_conn):
+            return True
+        else:
+            return False
 
     @commands.group()
     @commands.cooldown(1, 3, commands.BucketType.user)
     @commands.has_permissions(administrator=True)
     async def imgwelcome(self, ctx):
-        """IMG Welcomer"""
-        async with self.bot.sql_conn.acquire() as conn:
-            async with conn.cursor() as db:
-                if not await db.execute(f"SELECT 1 FROM newimgwelcome WHERE server = {ctx.message.guild.id}"):
-                    content = "Welcome {0} to {1}!"
-                    await db.execute(f"INSERT INTO newimgwelcome VALUES ({ctx.message.guild.id}, {ctx.message.channel.id}, \"{content}\")")
         if ctx.invoked_subcommand is None:
             em = discord.Embed(color=0xDEADBF, title="IMG Welcomer",
                                description="**n!imgwelcome img** - Set the background image\n"
                                            "**n!imgwelcome text** - Change the Welcome text above the image.\n"
                                            "**n!imgwelcome channel** - Set the welcome channel.\n"
-                                           "**n!imgwelcome disable** - Remove it from your server.")
+                                           "**n!imgwelcome toggle** - Toggle on/off.")
             await ctx.send(embed=em)
 
-    @imgwelcome.command(name="disable")
-    async def img_disable(self, ctx):
-        """Disable imgwelcome"""
-        async with self.bot.sql_conn.acquire() as conn:
-            async with conn.cursor() as db:
-                await db.execute(f"DELETE FROM newimgwelcome WHERE server = {ctx.message.guild.id}")
-        await ctx.send("Disabled imgwelcome...")
-
-    @imgwelcome.command(name="channel")
-    async def img_channel(self, ctx, channel:discord.TextChannel):
-        """Set the img text channel"""
-        async with self.bot.sql_conn.acquire() as conn:
-            async with conn.cursor() as db:
-                await db.execute("UPDATE newimgwelcome SET channel = %s WHERE server = %s", (channel.id, ctx.guild.id,))
-        await ctx.send(f"Updated channel to {channel.name}")
+    @imgwelcome.command(name="toggle")
+    async def imgwelcome_toggle(self, ctx):
+        """Toggle on/off the imgwelcomer"""
+        if await self.__is_enabled(ctx.guild.id):
+            await r.table("imgwelcome").get(str(ctx.guild.id)).delete().run(self.bot.r_conn)
+            await ctx.send("Disabled imgwelcoming")
+        else:
+            await r.table("imgwelcome").insert({
+                "id": str(ctx.guild.id),
+                "channel": str(ctx.channel.id),
+                "content": "V2VsY29tZSB1c2VyIHRvIHNlcnZlciE="
+            }).run(self.bot.r_conn)
+            await ctx.send("Enabled imgwelcoming")
 
     @imgwelcome.command(name="img")
-    async def img_img(self, ctx):
-        """Set the background welcome image"""
+    async def imgwelcome_img(self, ctx):
+        """Set the image"""
+        if not await self.__is_enabled(ctx.guild.id):
+            return await ctx.send("Enable imgwelcoming with n!imgwelcome toggle")
         await ctx.send("Send an image or type anything without sending an image to reset back to default.")
 
         def check(m):
@@ -58,8 +60,8 @@ class IMGWelcome:
             attachment = str(msg.attachments[0].url).rpartition(".")[2]
             if attachment.lower() not in ["png", "jpg", "jpeg", "gif"]:
                 return await ctx.send("Not a valid image type <:bakaa:432914537608380419>")
-            if os.path.exists(f"data/imgwelcome/{ctx.message.guild.id}.png"):
-                os.remove(f"data/imgwelcome/{ctx.message.guild.id}.png")
+            if os.path.exists(f"data/imgwelcome/{ctx.guild.id}.png"):
+                os.remove(f"data/imgwelcome/{ctx.guild.id}.png")
             try:
                 async with aiohttp.ClientSession() as cs:
                     async with cs.get(msg.attachments[0].url) as r:
@@ -67,73 +69,71 @@ class IMGWelcome:
                 img = Image.open(BytesIO(imgdata)).convert("RGBA").resize((500, 150))
                 bg = Image.new("RGBA", (500, 150), (0, 0, 0, 0))
                 bg.alpha_composite(img, (0, 0))
-                bg.save(f"data/imgwelcome/{ctx.message.guild.id}.png")
+                bg.save(f"data/imgwelcome/{ctx.guild.id}.png")
                 await ctx.send("Set image!")
             except Exception as e:
                 await ctx.send(f"Failed to set image... {e}")
         else:
-            if os.path.exists(f"data/imgwelcome/{ctx.message.guild.id}.png"):
-                os.remove(f"data/imgwelcome/{ctx.message.guild.id}.png")
+            if os.path.exists(f"data/imgwelcome/{ctx.guild.id}.png"):
+                os.remove(f"data/imgwelcome/{ctx.guild.id}.png")
             await ctx.send("Reset Image.")
 
-    def forbiddencheck(self, text:str):
-        characters = string.ascii_letters + string.digits + " '!<>@#:,.()-=&" + '"'
-        forbidden_char = 0
-        for letter in text:
-            if letter not in characters:
-                forbidden_char += 1
-        return forbidden_char
-
     @imgwelcome.command(name="text")
-    async def img_text(self, ctx, *, text:str):
-        """Set the image text, typing user will give the user as server will give the server name,
+    async def imgwelcome_text(self, ctx, *, text:str):
+        """Change the welcome text,
 
-        Welcome user to server!"""
-        newtext = text.replace("user", "{0}").replace("server", "{1}")
-        async with self.bot.sql_conn.acquire() as conn:
-            async with conn.cursor() as db:
-                await db.execute(f"UPDATE newimgwelcome SET content = %s WHERE server = %s", (newtext, ctx.message.guild.id,))
-        await ctx.send("Updated!")
+            user = the user's name
+            server = the server's name
+
+        Example:
+            n!imgwelcome text Welcome user to server!
+        """
+
+        if not await self.__is_enabled(ctx.guild.id):
+            return await ctx.send("Enable imgwelcoming with n!imgwelcome toggle")
+
+        text = (base64.b64encode(text.encode("utf8"))).decode("utf8")
+        await r.table("imgwelcome").get(str(ctx.guild.id)).update({"content": text}).run(self.bot.r_conn)
+        await ctx.send("Updating text!")
+
+    @imgwelcome.command(name="channel")
+    async def imgwelcome_channel(self, ctx, channel:discord.TextChannel):
+        """Select a channel to use for imgwelcoming, can be a channel mention or the exact name of it"""
+
+        if not await self.__is_enabled(ctx.guild.id):
+            return await ctx.send("Enable imgwelcoming with n!imgwelcome toggle")
+
+
+        await r.table("imgwelcome").get(str(ctx.guild.id)).update({"channel": str(channel.id)}).run(self.bot.r_conn)
+        await ctx.send("Updated to %s" % channel.name)
 
     @commands.command(hidden=True)
     @commands.is_owner()
-    async def imggen(self, ctx, member:discord.Member=None):
+    async def imggen(self, ctx, member: discord.Member = None):
         if member is None:
             member = ctx.message.author
         await self.on_member_join(member)
 
-    async def _circle_border(self, circle_img_size: tuple):
+    def _circle_border(self, circle_img_size: tuple):
         border_size = []
         for i in range(len(circle_img_size)):
             border_size.append(circle_img_size[0] + 8)
         return tuple(border_size)
 
-    def _get_suffix(self, num):
-        suffixes = {1: 'st', 2: 'nd', 3: 'rd'}
-        if 10 <= num % 100 <= 20:
-            suffix = 'th'
-        else:
-            suffix = suffixes.get(num % 10, 'th')
-        return suffix
-
     async def on_member_join(self, member):
-        server = member.guild
-        async with self.bot.sql_conn.acquire() as conn:
-            async with conn.cursor() as db:
-                if not await db.execute(f"SELECT 1 FROM newimgwelcome WHERE server = {server.id}"):
-                    return
-                else:
-                    await db.execute(f"SELECT server, channel, content FROM newimgwelcome WHERE server = {server.id}")
-                    alldata = await db.fetchall()
-                    channel = alldata[0][1]
-                    content = alldata[0][2]
-        channel = self.bot.get_channel(int(channel))
+        guild = member.guild
+        if not await self.__is_enabled(guild.id):
+            return
+
+        data = await rethonk.table("imgwelcome").get(str(guild.id)).run(self.bot.r_conn)
+
+        channel = self.bot.get_channel(int(data["channel"]))
         if not channel:
             return
         await channel.trigger_typing()
 
-        if os.path.exists(f"data/imgwelcome/{server.id}.png"):
-            background = Image.open(f"data/imgwelcome/{server.id}.png").convert("RGBA")
+        if os.path.exists(f"data/imgwelcome/{guild.id}.png"):
+            background = Image.open(f"data/imgwelcome/{guild.id}.png").convert("RGBA")
         else:
             background = Image.open("data/imgwelcome/transparent.png")
 
@@ -158,8 +158,8 @@ class IMGWelcome:
         draw_thumb.ellipse((0, 0) + (512, 512), fill=255, outline=0)
         circle = Image.new("RGBA", (512, 512))
         draw_circle = ImageDraw.Draw(circle)
-        draw_circle.ellipse([0, 0, 512, 512], fill=(255,255, 255, 180), outline=(255, 255, 255, 250))
-        circle_border_size = await self._circle_border((128, 128))
+        draw_circle.ellipse([0, 0, 512, 512], fill=(255, 255, 255, 180), outline=(255, 255, 255, 250))
+        circle_border_size = self._circle_border((128, 128))
         circle = circle.resize((circle_border_size), Image.ANTIALIAS)
         circle_mask = mask.resize((circle_border_size), Image.ANTIALIAS)
         circle_pos = (7 + int((136 - circle_border_size[0]) / 2))
@@ -214,29 +214,16 @@ class IMGWelcome:
             drawtwo.text((152, 73), uname, 1, name_font_smallest, (0, 0, 0, 255))
             drawtwo.text((152, 73), uname, font=name_font_smallest, fill=(255, 255, 255, 230))
 
-        # try:
-        #     members = sorted(server.members, key=lambda m: m.joined_at).index(member) + 1
-        # except:
-        #     members = len(server.members)
-
-        # member_number = str(members) + self._get_suffix(members)
-        # sname = str(member.guild.name) + '!' if len(str(member.guild.name)) <= 28 else str(member.guild.name)[
-        #                                                                                  :23] + '...'
-
-        # _outline((152, 96), "You are the " + str(member_number) + " member", 1, server_font, (0, 0, 0, 255))
-        # drawtwo.text((152, 96), "You are the " + str(member_number) + " member", font=server_font, fill=(255, 255, 255, 230))
-        # _outline((152, 116), 'of ' + sname, 1, server_font, (0, 0, 0, 255))
-        # drawtwo.text((152, 116), 'of ' + sname, font=server_font, fill=(255, 255, 255, 230))
-
-        server_text = f"Welcome to {server.name}!"
-        server_text = textwrap.wrap(server_text, 25)
+        server_text = textwrap.wrap(f"Welcome to {guild.name}!", 25)[0]
         _outline((152, 100), server_text, 1, server_font, (0, 0, 0, 255))
         drawtwo.text((152, 100), server_text, font=server_font, fill=(255, 255, 255, 230))
 
         welcome_picture.save("data/welcome.png")
 
+        content = ((base64.b64decode(str(data["content"]).encode("utf8"))).decode("utf8")).replace("user", member.name).replace("server", guild.name)
+
         file = discord.File("data/welcome.png", filename="welcome.png")
-        await channel.send(file=file, content=content.format(member.name, member.guild.name))
+        await channel.send(file=file, content=content)
 
 def setup(bot):
     bot.add_cog(IMGWelcome(bot))
