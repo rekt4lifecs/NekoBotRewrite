8b1aa5704b412b5f5220c8d9cb9d51a33b066d7a
ReKT <20015683+ReKTDevlol@users.noreply.github.com>
2018-08-30 20:05:27 +1000

    tsk

--- shardedBot.py
+++ shardedBot.py
@@ -179,9 +179,9 @@ class NekoBot(commands.AutoShardedBot):
                 await self.redis.set("instance%s-channels" % self.instance, len(set(self.get_all_channels())))
                 logger.info(f"Updated Instance {self.instance}'s Guild Count with {len(self.guilds)}")
 
-                top_users = await r.table("economy").order_by(r.desc("balance")).limit(10).run(self.r_conn)
-
                 if self.instance == 0:
+
+                    top_users = await r.table("economy").order_by(r.desc("balance")).limit(10).run(self.r_conn)
 
                     for i, u in enumerate(top_users):
                         try:
