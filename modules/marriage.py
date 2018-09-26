a33665089119a1f5e0a1e4b7e6ec6efc8a4be3e3
ReKT <20015683+ReKTDevlol@users.noreply.github.com>
2018-09-27 05:57:57 +1000

    marriage strings update

--- modules/marriage.py
+++ modules/marriage.py
@@ -1,45 +1,42 @@
 from discord.ext import commands
 import discord, asyncio
-import ujson
+import gettext
 import rethinkdb as r
 from .utils import chat_formatting
-
-# Languages
-languages = ["english", "weeb", "tsundere", "polish", "spanish", "french"]
-lang = {}
-
-for l in languages:
-    with open("lang/%s.json" % l, encoding="utf-8") as f:
-        lang[l] = ujson.load(f)
-
-def getlang(la:str):
-    return lang.get(la, None)
 
 class Marriage:
 
     def __init__(self, bot):
         self.bot = bot
+        self.lang = {}
+        for x in ["french", "polish", "spanish", "tsundere", "weeb"]:
+            self.lang[x] = gettext.translation("marriage", localedir="locale", languages=[x])
+
+    async def _get_text(self, ctx):
+        lang = await self.bot.get_language(ctx)
+        if lang:
+            return self.lang[lang].gettext
+        else:
+            return gettext.gettext
 
     @commands.command()
-    @commands.cooldown(1, 10, commands.BucketType.user)
+    @commands.cooldown(1, 7, commands.BucketType.user)
     async def marry(self, ctx, user : discord.Member):
         """Marry someone OwO"""
         author = ctx.author
 
-        lang = await self.bot.redis.get(f"{ctx.author.id}-lang")
-        if lang:
-            lang = lang.decode("utf8")
-        else:
-            lang = "english"
+        _ = await self._get_text(ctx)
 
         if user == author:
-            return await ctx.send(chat_formatting.bold(getlang(lang)["marriage"]["marry_self"]))
+            return await ctx.send(chat_formatting.bold(_("You can't marry yourself.")))
         if await r.table("marriage").get(str(author.id)).run(self.bot.r_conn):
-            return await ctx.send(chat_formatting.bold(getlang(lang)["marriage"]["author_married"]))
+            return await ctx.send(chat_formatting.bold(_("You are already married")))
         elif await r.table("marriage").get(str(user.id)).run(self.bot.r_conn):
-            return await ctx.send(chat_formatting.bold(getlang(lang)["marriage"]["user_married"]))
+            return await ctx.send(chat_formatting.bold(_("That user is already married")))
         else:
-            await ctx.send(getlang(lang)["marriage"]["marry_msg"].format(author, user))
+            a_name = author.name.replace("@", "@\u200B")
+            u_name = user.name.replace("@", "@\u200B")
+            await ctx.send(_("%s is wanting to marry %s!\n%s type yes to accept!") % (a_name, u_name, user.mention))
 
             def check(m):
                 return m.channel == ctx.message.channel and m.author == user
@@ -47,9 +44,9 @@ class Marriage:
             try:
                 msg = await self.bot.wait_for("message", check=check, timeout=15.0)
                 if msg.content.lower() != "yes":
-                    return await ctx.send(embed=discord.Embed(color=0xff5630, description=getlang(lang)["marriage"]["cancelled"]))
+                    return await ctx.send(embed=discord.Embed(color=0xff5630, description=_("Marriage Cancelled.")))
             except asyncio.TimeoutError:
-                await ctx.send(embed=discord.Embed(color=0xff5630, description=getlang(lang)["marriage"]["cancelled"]))
+                await ctx.send(embed=discord.Embed(color=0xff5630, description=_("Marriage Cancelled.")))
                 return
 
             await ctx.send(f"🎉 {author.mention} ❤ {user.mention} 🎉")
@@ -57,37 +54,33 @@ class Marriage:
             await r.table("marriage").insert({"id": str(user.id), "marriedTo": str(author.id)}).run(self.bot.r_conn)
 
     @commands.command()
-    @commands.cooldown(1, 10, commands.BucketType.user)
+    @commands.cooldown(1, 7, commands.BucketType.user)
     async def divorce(self, ctx):
         """Divorce ;-;"""
-        author = ctx.message.author
+        author = ctx.author
 
-        lang = await self.bot.redis.get(f"{ctx.author.id}-lang")
-        if lang:
-            lang = lang.decode("utf8")
-        else:
-            lang = "english"
+        _ = await self._get_text(ctx)
 
         if not await r.table("marriage").get(str(author.id)).run(self.bot.r_conn):
-            return await ctx.send(chat_formatting.bold(getlang(lang)["marriage"]["not_married"]))
+            return await ctx.send(chat_formatting.bold(_("You are not married")))
         x = await r.table("marriage").get(str(author.id)).run(self.bot.r_conn)
         user_married_to = int(x["marriedTo"])
-        married_to_name = await self.bot.get_user_info(user_married_to)
+        married_to_name = (await self.bot.get_user_info(user_married_to)).replace("@", "@\u200B")
 
         def check(m):
             return m.channel == ctx.message.channel and m.author == author
 
-        await ctx.send("**Are you sure you want to divorce %s?**" % (married_to_name,))
+        await ctx.send(_("**Are you sure you want to divorce %s?**") % (married_to_name,))
 
         try:
             msg = await self.bot.wait_for("message", check=check, timeout=15.0)
             if msg.content.lower() != "yes":
-                return await ctx.send("**Cancelled.**")
+                return await ctx.send(_("**Cancelled.**"))
         except asyncio.TimeoutError:
-            await ctx.send("**Cancelled.**")
+            await ctx.send(_("**Cancelled.**"))
             return
 
-        await ctx.send(f"{author.name} divorced {married_to_name} 😦😢")
+        await ctx.send(_("%s divorced %s 😦😢") % (author.name.replace("@", "@\u200B"), married_to_name))
         await r.table("marriage").get(str(author.id)).delete().run(self.bot.r_conn)
         await r.table("marriage").get(str(user_married_to)).delete().run(self.bot.r_conn)
 
