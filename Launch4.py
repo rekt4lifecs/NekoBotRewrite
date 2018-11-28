a786868887eafcd1d6a58265c30a64d65982e4ff
ReKT <20015683+ReKTDevlol@users.noreply.github.com>
2018-11-28 14:26:11 +1100

    shardsweary

--- Launch4.py
+++ Launch4.py
@@ -3,6 +3,6 @@ import shardedBot
 if __name__ == "__main__":
     instance = 3
     instances = 4
-    shards = 100
-    shard_ids = [75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99]
+    shards = 112
+    shard_ids = [84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111]
     shardedBot.NekoBot(instance=instance, instances=instances, shard_count=shards, shard_ids=shard_ids, max_messages=101).run()
