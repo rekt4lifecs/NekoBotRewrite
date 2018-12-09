13d0efd66381a8e7beec0e3c19bf78e0d5995525
ReKT <20015683+ReKTDevlol@users.noreply.github.com>
2018-12-09 15:05:31 +1100

    weary

--- modules/discordbots.py
+++ modules/discordbots.py
@@ -5,7 +5,7 @@ from .utils import instance_tools
 
 log = logging.getLogger()
 
-stats2 = ["OwO whats n!help", "🤔🤔🤔", "👀", "(╯°□°）╯︵ ┻━┻",
+statuses = ["OwO whats n!help", "🤔🤔🤔", "👀", "(╯°□°）╯︵ ┻━┻",
           "¯\_(ツ)_/¯", "┬─┬ノ(ಠ_ಠノ)", "><(((('>", "_/\__/\__0>", "ô¿ô", "°º¤ø,¸¸,ø¤º°`°º¤ø,", "=^..^=",
           "龴ↀ◡ↀ龴", "^⨀ᴥ⨀^", "^⨀ᴥ⨀^", "⨌⨀_⨀⨌", "•|龴◡龴|•", "ˁ˚ᴥ˚ˀ", "⦿⽘⦿", " (╯︵╰,)",
           " (╯_╰)", "㋡", "ˁ˚ᴥ˚ˀ", "\(^-^)/", "uwu", ":lurk:", "b-baka >_<",
@@ -19,7 +19,7 @@ class DiscordBotsOrgAPI:
         self.has_started = 0
         self.token = config.dbots_key
 
-    async def startdbl(self):
+    async def postloop(self):
         if not self.has_started == 1:
             self.has_started = 1
             while True:
@@ -29,49 +29,46 @@ class DiscordBotsOrgAPI:
                 i = instance_tools.InstanceTools(self.bot.instances, self.bot.redis)
                 guilds = await i.get_all_guilds()
 
-                game = discord.Streaming(name=random.choice(stats2), url="https://www.twitch.tv/nekoboat")
+                game = discord.Streaming(name=random.choice(statuses), url="https://www.twitch.tv/nekoboat")
                 await self.bot.change_presence(activity=game)
                 log.info("Servers: %s" % guilds)
                 if self.bot.instance == 0:
-                    try:
-                        url = "https://discordbots.org/api/bots/310039170792030211/stats"
-                        payload = {
-                            "server_count": int(guilds),
-                            "shard_count": self.bot.shard_count
-                        }
-                        async with aiohttp.ClientSession() as cs:
-                            await cs.post(url, json=payload, headers={"Authorization": config.dbots_key})
-                        log.info("Posted server count. {}".format(guilds))
-                    except Exception as e:
-                        log.error('Failed to post server count\n{}: {}'.format(type(e).__name__, e))
 
-                    try:
-                        async with aiohttp.ClientSession() as session:
-                            await session.post("https://discord.bots.gg/api/v1/bots/310039170792030211/stats",
-                                                    headers={'Authorization': f'{config.dpw_key}'},
-                                                    json={"guildCount": int(guilds),
-                                                          "shardCount": self.bot.shard_count})
-                    except Exception as e:
-                        log.error(f"Failed to post to pw, {e}")
-                    try:
-                        url = "https://discord.services/api/bots/310039170792030211"
-                        payload = {
-                            "guild_count": int(guilds)
-                        }
-                        async with aiohttp.ClientSession() as cs:
-                            await cs.post(url, json=payload, headers={"Authorization": config.ds_key})
-                    except Exception as e:
-                        log.error(f"Failed to post to ds, {e}")
-                # try:
-                #     url = f"https://listcord.com/api/bot/{self.bot.user.id}/guilds"
-                #     async with aiohttp.ClientSession() as cs:
-                #         await cs.post(url, headers={"Authorization": config.listcord}, json={"guilds": int(guilds)})
-                # except Exception as e:
-                #     log.error(f"Failed to post to listcord, {e}")
+                    async with aiohttp.ClientSession() as cs:
+                        await cs.post(
+                            "https://discordbots.org/api/bots/310039170792030211/stats",
+                            json={
+                                "server_count": int(guilds),
+                                "shard_count": self.bot.shard_count
+                            },
+                            headers={
+                                "Authorization": config.dbots_key
+                            }
+                        )
+                        await cs.post(
+                            "https://discord.bots.gg/api/v1/bots/310039170792030211/stats",
+                            json={
+                                "guildCount": int(guilds),
+                                "shardCount": self.bot.shard_count
+                            },
+                            headers={
+                                "Authorization": config.dpw_key
+                            }
+                        )
+                        await cs.post(
+                            "https://discord.services/api/bots/310039170792030211",
+                            json={
+                                "guild_count": int(guilds)
+                            },
+                            headers={
+                                "Authorization": config.ds_key
+                            }
+                        )
+
                 await asyncio.sleep(1800)
 
     async def on_ready(self):
-        await self.startdbl()
+        await self.postloop()
 
 def setup(bot):
     bot.add_cog(DiscordBotsOrgAPI(bot))
