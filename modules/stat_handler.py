e9a00f4d4a352fb828cb570b228e8392428c99a2
hibikidesu <20015683+ReKTDevlol@users.noreply.github.com>
2019-02-22 15:44:58 +1100

    weary

--- modules/stat_handler.py
+++ modules/stat_handler.py
@@ -5,37 +5,27 @@ from .utils import instance_tools
 
 log = logging.getLogger()
 
-statuses = ["OwO whats n!help", "🤔🤔🤔", "👀", "(╯°□°）╯︵ ┻━┻",
-          "¯\_(ツ)_/¯", "┬─┬ノ(ಠ_ಠノ)", "><(((('>", "_/\__/\__0>", "ô¿ô", "°º¤ø,¸¸,ø¤º°`°º¤ø,", "=^..^=",
-          "龴ↀ◡ↀ龴", "^⨀ᴥ⨀^", "^⨀ᴥ⨀^", "⨌⨀_⨀⨌", "•|龴◡龴|•", "ˁ˚ᴥ˚ˀ", "⦿⽘⦿", " (╯︵╰,)",
-          " (╯_╰)", "㋡", "ˁ˚ᴥ˚ˀ", "\(^-^)/", "uwu", ":lurk:", "b-baka >_<",
-          "I-it's not like I like you or anything!", "ばか >_<", "(//・.・//)", "....T-Thanks.....",
-          "Hmph"]
-
 class StatHandler:
 
     def __init__(self, bot):
         self.bot = bot
         self.has_started = 0
-        self.token = config.dbots_key
 
     async def postloop(self):
         if not self.has_started == 1:
             self.has_started = 1
-            while True:
+            while self.has_started:
                 log.info("Getting all servers.")
                 log.info("Attempting to update server count.")
 
                 i = instance_tools.InstanceTools(self.bot.instances, self.bot.redis)
                 guilds = await i.get_all_guilds()
 
-                # game = discord.Streaming(name=random.choice(statuses), url="https://www.twitch.tv/nekoboat")
-                # await self.bot.change_presence(activity=game)
                 log.info("Servers: %s" % guilds)
                 if self.bot.instance == 0:
 
                     async with aiohttp.ClientSession() as cs:
-                        await cs.post(
+                        x = await cs.post(
                             "https://discordbots.org/api/bots/310039170792030211/stats",
                             json={
                                 "server_count": int(guilds),
@@ -45,7 +35,8 @@ class StatHandler:
                                 "Authorization": config.dbots_key
                             }
                         )
-                        await cs.post(
+                        log.info("Posted to discordbots.org, {}".format(await x.json()))
+                        x = await cs.post(
                             "https://discord.bots.gg/api/v1/bots/310039170792030211/stats",
                             json={
                                 "guildCount": int(guilds),
@@ -55,6 +46,7 @@ class StatHandler:
                                 "Authorization": config.dpw_key
                             }
                         )
+                        log.info("Posted to discord.bots.gg, {}".format(await x.json()))
                         await cs.post(
                             "https://discord.services/api/bots/310039170792030211",
                             json={
@@ -64,11 +56,23 @@ class StatHandler:
                                 "Authorization": config.ds_key
                             }
                         )
+                        log.info("Posted to discord.services, {}".format(await x.json()))
+                        await cs.post(
+                            "https://lbots.org/api/v1/bots/310039170792030211/stats",
+                            json={
+                                "guild_count": int(guilds),
+                                "shard_count": self.bot.shard_count
+                            },
+                            headers={
+                                "Authorization": config.lbots_key
+                            }
+                        )
+                        log.info("Posted to lbots.org, {}".format(await x.json()))
 
                 await asyncio.sleep(1800)
 
-    # async def on_ready(self):
-    #     await self.postloop()
+    async def on_ready(self):
+        self.bot.loop.create_task(self.postloop())
 
 def setup(bot):
     bot.add_cog(StatHandler(bot))
