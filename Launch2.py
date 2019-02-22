4b42cc5184e758c45f9dfc4afba986fdc4c66192
hibikidesu <20015683+ReKTDevlol@users.noreply.github.com>
2019-02-22 15:46:31 +1100

    unused

--- Launch2.py
+++
@@ -1,8 +0,0 @@
-import shardedBot
-
-if __name__ == "__main__":
-    instance = 1
-    instances = 4
-    shards = 112
-    shard_ids = [28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55]
-    shardedBot.NekoBot(instance=instance, instances=instances, shard_count=shards, shard_ids=shard_ids, max_messages=101).run()
