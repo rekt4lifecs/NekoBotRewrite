923bb727074212bc9115020169b1825ede6a8047
hibikidesu <20015683+ReKTDevlol@users.noreply.github.com>
2019-03-17 01:39:51 +1100

    Update cooldown.py

--- modules/utils/cooldown.py
+++ modules/utils/cooldown.py
@@ -4,11 +4,11 @@ from time import time
 def Cooldown(cooldown_time: int):
     def wrapper(func):
         @wraps(func)
-        async def wrapped(ctx, args):
+        async def wrapped(ctx, *args):
             data = await ctx.bot.redis.get("cooldown:{}:{}".format(ctx.command.name, ctx.author.id))
             if data is not None:
                 raise ValueError("Command is on cooldown... {}s left".format(int(data) - int(time())))
             await ctx.bot.redis.set("cooldown:{}:{}".format(ctx.command.name, ctx.author.id), int(time() + cooldown_time), expire=cooldown_time)
-            return await func(ctx, args)
+            return await func(ctx, *args)
         return wrapped
     return wrapper
