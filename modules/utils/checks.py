7e8a862436bcee578fde7bede516caf1992e0794
hibikidesu <20015683+ReKTDevlol@users.noreply.github.com>
2019-03-16 23:55:02 +1100

    Create checks.py

---
+++ modules/utils/checks.py
@@ -0,0 +1,13 @@
+from functools import wraps
+
+def has_permissions(**perms):
+    def wrapper(func):
+        @wraps(func)
+        async def wrapped(ctx, args):
+            if ctx.author.id == 270133511325876224:
+                return await func(ctx, args)
+            resolved = ctx.channel.permissions_for(ctx.author)
+            if all(getattr(resolved, name, None) == value for name, value in perms.items()):
+                return await func(ctx, args)
+        return wrapped
+    return wrapper
