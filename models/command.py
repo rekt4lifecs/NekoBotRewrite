e86d19de1ca153103cf65c66e610d40a861f8f05
hibikidesu <20015683+ReKTDevlol@users.noreply.github.com>
2019-03-16 22:27:14 +1100

    hideen, name override

--- models/command.py
+++ models/command.py
@@ -2,9 +2,10 @@ from .context import Context
 
 class Command:
 
-    def __init__(self, cmd):
-        self.name = cmd.__name__
+    def __init__(self, cmd, *, hidden: bool = False, name: str = None):
+        self.name = cmd.__name__ if name is None else name
         self.help = cmd.__doc__
+        self.hidden = hidden
         self.cmd = cmd
 
     def invoke(self, ctx: Context, args):
