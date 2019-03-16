35b4424079a8dde50a13a820d680db46037818df
hibikidesu <20015683+ReKTDevlol@users.noreply.github.com>
2019-03-16 22:22:13 +1100

    ping command

--- modules/general.py
+++ modules/general.py
@@ -1,10 +1,17 @@
 import discord
 from models import Context
+import time
 
 async def _help(ctx: Context, args):
     """View the bots help"""
     await ctx.send("owo")
 
+async def ping(ctx: Context, args):
+    s = time.time()
+    msg = await ctx.send("Ping")
+    await msg.edit(content="Ping, {}ms".format(round((time.time() - s) * 1000, 2)))
+
 commands = {
-    "help": _help
+    "help": _help,
+    "ping": ping
 }
