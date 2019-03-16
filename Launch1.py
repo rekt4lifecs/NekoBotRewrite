07dcf8449558a8ecd9af8e4f3f6099e7c6fc684e
hibikidesu <20015683+ReKTDevlol@users.noreply.github.com>
2019-03-16 17:48:12 +1100

    weary

--- Launch1.py
+++
@@ -1,8 +0,0 @@
-
-import shardedBot2
-
-class Pipe:
-    closed = True
-
-if __name__ == "__main__":
-    shardedBot2.NekoBot(1, 8, 128, [16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31], Pipe(), None)
--- Launch7.py
+++
@@ -1,8 +0,0 @@
-
-import shardedBot2
-
-class Pipe:
-    closed = True
-
-if __name__ == "__main__":
-    shardedBot2.NekoBot(7, 8, 128, [112, 113, 114, 115, 116, 117, 118, 119, 120, 121, 122, 123, 124, 125, 126, 127], Pipe(), None)
