4023f1a164f3dddd2cf57ad8a22a2f2b285eeb20
hibikidesu <20015683+ReKTDevlol@users.noreply.github.com>
2019-03-17 13:06:02 +1100

    Update Launcher.py

--- Launcher.py
+++ Launcher.py
@@ -5,8 +5,8 @@ import shutil
 import os
 from signal import SIGKILL
 
-shards = 128
-shards_per_instance = 16
+shards = 96
+shards_per_instance = 24
 instances = int(shards / shards_per_instance)
 processes = list()
 
@@ -44,22 +44,22 @@ if __name__ == "__main__":
 
     try:
         while True:
-            # try:
-            #     for powo in processes_owo:
-            #         proc = processes_owo[powo].get("process")
-            #         if not proc.is_alive():
-            #             listen, send = Pipe()
-            #             p = Process(target=bot.NekoBot, args=(int(powo), instances, shards, processes_owo[powo]["ids"], send, ipc_queue))
-            #             p.start()
-            #             processes_owo[powo]["process"] = p
-            #             print("Relaunched {}".format(powo))
-            #             processes.append(p.pid)
-            #
-            #             if listen.recv() == 1:
-            #                 print("Instance {} Launched".format(p))
-            #             listen.close()
-            # except Exception as e:
-            #     print("Failed to restart process, {}".format(e))
+            try:
+                for powo in processes_owo:
+                    proc = processes_owo[powo].get("process")
+                    if not proc.is_alive():
+                        listen, send = Pipe()
+                        p = Process(target=bot.NekoBot, args=(int(powo), instances, shards, processes_owo[powo]["ids"], send, ipc_queue))
+                        p.start()
+                        processes_owo[powo]["process"] = p
+                        print("Relaunched {}".format(powo))
+                        processes.append(p.pid)
+
+                        if listen.recv() == 1:
+                            print("Instance {} Launched".format(p))
+                        listen.close()
+            except Exception as e:
+                print("Failed to restart process, {}".format(e))
             wait(5)
     except KeyboardInterrupt:
         for process in processes:
