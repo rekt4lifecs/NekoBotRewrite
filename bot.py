e63ed0e83ac1d72ea929458da42f1cc34a61dae2
hibikidesu <20015683+ReKTDevlol@users.noreply.github.com>
2019-03-17 13:06:05 +1100

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,7 +1,84 @@
 from discord.ext import commands
 import discord
+import config
+import logging
+import sys, time
+
+
+BLACK, RED, GREEN, YELLOW, BLUE, MAGENTA, CYAN, WHITE = range(8)
+RESET_SEQ = "\033[0m"
+COLOR_SEQ = "\033[1;%dm"
+BOLD_SEQ = "\033[1m"
+TIME_SEQ = COLOR_SEQ % (30 + MAGENTA)
+NAME_SEQ = COLOR_SEQ % (30 + CYAN)
+FORMAT = "[$TIME_SEQ%(asctime)-3s$RESET]" \
+         "[$NAME_SEQ$BOLD%(name)-2s$RESET]" \
+         "[%(levelname)-1s]" \
+         "[%(message)s]" \
+         "[($BOLD%(filename)s$RESET:%(lineno)d)]"
+
+def formatter_message(message: str, colored: bool = True):
+    if colored:
+        message = message.replace("$RESET", RESET_SEQ)
+        message = message.replace("$BOLD", BOLD_SEQ)
+        message = message.replace("$TIME_SEQ", TIME_SEQ)
+        message = message.replace("$NAME_SEQ", NAME_SEQ)
+        return message
+    else:
+        message = message.replace("$RESET", "")
+        message = message.replace("$BOLD", "")
+        return message
+
+class ColoredFormatter(logging.Formatter):
+    def __init__(self, msg, use_color=True):
+        logging.Formatter.__init__(self, msg)
+        self.use_color = use_color
+
+    def format(self, record):
+        level_name = record.levelname
+        if self.use_color and level_name in COLORS:
+            level_name_color = COLOR_SEQ % (30 + COLORS[level_name]) + level_name + RESET_SEQ
+            record.levelname = level_name_color
+        message = record.msg
+        if self.use_color and level_name in COLORS:
+            message_color = COLOR_SEQ % (30 + BLUE) + message + RESET_SEQ
+            record.msg = message_color
+        return logging.Formatter.format(self, record)
+
+class ColoredLogger(logging.Logger):
+    def __init__(self, name):
+        logging.Logger.__init__(self, name, logging.INFO)
+        return
+
+COLORS = {
+    'WARNING': YELLOW,
+    'INFO': BLUE,
+    'DEBUG': WHITE,
+    'CRITICAL': YELLOW,
+    'ERROR': RED
+}
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+color_format = formatter_message(FORMAT, True)
+logging.setLoggerClass(ColoredLogger)
+color_formatter = ColoredFormatter(color_format)
+console = logging.StreamHandler()
+console.setFormatter(color_formatter)
+logger.addHandler(console)
+
+if sys.platform == "linux":
+    file = logging.FileHandler(filename=f"logs/{str(time.time())}.log", encoding="utf-8", mode="w")
+    file.setFormatter(color_formatter)
+    logger.addHandler(file)
 
 class NekoBot(commands.AutoShardedBot):
 
     def __init__(self):
         super().__init__()
+
+    def run(self, token: str = config.token):
+        super().run(token)
+
+if __name__ == "__main__":
+    NekoBot().run()
