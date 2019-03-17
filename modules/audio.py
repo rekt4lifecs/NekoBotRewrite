7bb9b9d41b149c092d3dae8d67b450de84e175da
hibikidesu <20015683+ReKTDevlol@users.noreply.github.com>
2019-03-17 13:50:21 +1100

    Update audio.py

--- modules/audio.py
+++ modules/audio.py
@@ -32,21 +32,6 @@ class Audio:
             self.bot.loop.create_task(self.__post_to_hook("Registered hook"))
             self.bot.lavalink.register_hook(self._track_hook)
 
-        self.lang = {}
-        self.languages = ["tsundere", "weeb", "chinese"]
-        for x in self.languages:
-            self.lang[x] = gettext.translation("general", localedir="locale", languages=[x])
-
-    async def _get_text(self, ctx):
-        lang = await self.bot.get_language(ctx)
-        if lang:
-            if lang in self.languages:
-                return self.lang[lang].gettext
-            else:
-                return gettext.gettext
-        else:
-            return gettext.gettext
-
     async def __post_to_hook(self, message):
         async with aiohttp.ClientSession() as cs:
             await cs.post(config.lavalink_hook, json={
@@ -110,26 +95,25 @@ class Audio:
     @commands.guild_only()
     @commands.cooldown(1, 6, commands.BucketType.user)
     async def play(self, ctx, *, query: str):
-        _ = await self._get_text(ctx)
 
         player = self.bot.lavalink.players.get(ctx.guild.id)
 
         if not player.is_connected:
             if not ctx.author.voice or not ctx.author.voice.channel:
-                return await ctx.send(_("You are not in any voice channel ;w;"))
+                return await ctx.send("You are not in any voice channel ;w;")
 
             permissions = ctx.author.voice.channel.permissions_for(ctx.me)
 
             if not permissions.connect or not permissions.speak:
-                return await ctx.send(_("Missing permissions to connect or speak ;w;"))
+                return await ctx.send("Missing permissions to connect or speak ;w;")
             player.store("channel", ctx.channel.id)
             await player.connect(ctx.author.voice.channel.id)
         else:
             if player.connected_channel.id != ctx.author.voice.channel.id:
-                return await ctx.send(_("Join my voice channel you baka"))
+                return await ctx.send("Join my voice channel you baka")
 
         if len(player.queue) > 50:
-            return await ctx.send(_("You are too much songs in your queue you baka"))
+            return await ctx.send("You are too much songs in your queue you baka")
 
         query = query.strip("<>")
 
@@ -138,43 +122,43 @@ class Audio:
         elif "osu.ppy.sh/beatmapsets" in query or "hentaihaven.org/" in query:
             await ctx.trigger_typing()
             if "hentaihaven" in query and not ctx.channel.is_nsfw():
-                return await ctx.send(_("This is not an nsfw channel hmph shouldn't be posting that here >:|"))
+                return await ctx.send("This is not an nsfw channel hmph shouldn't be posting that here >:|")
             data = {"url": query}
             async with aiohttp.ClientSession() as cs:
-                async with cs.post("https://lava.nekobot.xyz/api", json=data, headers=ll_headers) as r:
-                    res = await r.json()
+                async with cs.post("https://lava.nekobot.xyz/api", json=data, headers=ll_headers) as res:
+                    res = await res.json()
             if not res["message"].startswith("http"):
-                return await ctx.send(_("Failed to get data ;w;"))
+                return await ctx.send("Failed to get data ;w;")
             query = res["message"]
 
         results = await self.bot.lavalink.get_tracks(query)
 
         if not results or not results["tracks"]:
-            return await ctx.send(_("I found nothing ;w;"))
+            return await ctx.send("I found nothing ;w;")
 
         if results["loadType"] == "PLAYLIST_LOADED":
             tracks = results["tracks"]
 
             if len(tracks) > 50:
-                return await ctx.send(_("Too much tracks in this playlist ;w;"))
+                return await ctx.send("Too much tracks in this playlist ;w;")
             elif (len(tracks) + len(player.queue)) > 50:
-                return await ctx.send(_("Too much in queue already ;w;"))
+                return await ctx.send("Too much in queue already ;w;")
 
             for track in tracks:
                 if not track["info"]["length"] > 3600000:
                     player.add(requester=ctx.author.id, track=track)
 
-            await ctx.send(_("Added playlist **%s**") % results["playlistInfo"]["name"].replace("@", "@\u200B"))
+            await ctx.send("Added playlist **%s**" % results["playlistInfo"]["name"].replace("@", "@\u200B"))
         else:
             if len(results["tracks"]) < 2:
                 track = results["tracks"][0]
                 if track["info"]["length"] > 3600000:
                     return await ctx.send("Thats too long for me to play baka")
-                await ctx.send(_("Added **%s** to queue") % track["info"]["title"].replace("@", "@\u200B"))
+                await ctx.send("Added **%s** to queue" % track["info"]["title"].replace("@", "@\u200B"))
                 player.add(requester=ctx.author.id, track=track)
             else:
                 tracks = results["tracks"][:5]
-                msg = _("Type a number of a track to play.```\n")
+                msg = "Type a number of a track to play.```\n"
                 for i, track in enumerate(tracks, start=1):
                     msg += "%s. %s\n" % (i, track["info"]["title"].replace("@", "@\u200B"))
                 msg += "```"
@@ -195,15 +179,15 @@ class Audio:
                 try:
                     x = int(x.content)
                 except:
-                    return await ctx.send(_("Not a valid number, returning"))
+                    return await ctx.send("Not a valid number, returning")
                 if x not in list(range(1, len(tracks) + 1)):
-                    return await ctx.send(_("Not a valid option, returning"))
+                    return await ctx.send("Not a valid option, returning")
 
                 track = tracks[x - 1]
                 if track["info"]["length"] > 3600000:
                     return await ctx.send("Thats too long for me to play baka")
 
-                await ctx.send(_("Added **%s** to queue") % track["info"]["title"].replace("@", "@\u200B"))
+                await ctx.send("Added **%s** to queue" % track["info"]["title"].replace("@", "@\u200B"))
                 player.add(requester=ctx.author.id, track=track)
 
             if not player.is_playing:
@@ -213,41 +197,39 @@ class Audio:
     @commands.guild_only()
     @commands.cooldown(1, 3, commands.BucketType.user)
     async def listenmoe(self, ctx):
-        _ = await self._get_text(ctx)
 
         player = self.bot.lavalink.players.get(ctx.guild.id)
 
         if not player.is_connected:
             if not ctx.author.voice or not ctx.author.voice.channel:
-                return await ctx.send(_("You are not in any voice channel ;w;"))
+                return await ctx.send("You are not in any voice channel ;w;")
 
             permissions = ctx.author.voice.channel.permissions_for(ctx.me)
 
             if not permissions.connect or not permissions.speak:
-                return await ctx.send(_("Missing permissions to connect or speak ;w;"))
+                return await ctx.send("Missing permissions to connect or speak ;w;")
             player.store('channel', ctx.channel.id)
             await player.connect(ctx.author.voice.channel.id)
         else:
             if player.connected_channel.id != ctx.author.voice.channel.id:
-                return await ctx.send(_("Join my voice channel you baka"))
+                return await ctx.send("Join my voice channel you baka")
             if player.is_playing:
                 player.queue.clear()
 
         track = (await self.bot.lavalink.get_tracks("https://listen.moe/stream"))["tracks"][0]
         player.add(requester=ctx.author.id, track=track)
         await player.play()
-        await ctx.send(_("Playing uwu"))
+        await ctx.send("Playing uwu")
 
     @commands.command()
     @commands.guild_only()
     @commands.cooldown(1, 3, commands.BucketType.user)
     async def volume(self, ctx, amount: int):
         """Change the volume"""
-        _ = await self._get_text(ctx)
         player = self.bot.lavalink.players.get(ctx.guild.id)
 
         if not player.is_connected:
-            return await ctx.send(_("I'm not even playing anything"))
+            return await ctx.send("I'm not even playing anything")
         else:
             if player.connected_channel.id != ctx.author.voice.channel.id:
                 return await ctx.send("Join my voice channel you baka")
@@ -255,35 +237,33 @@ class Audio:
         amount = max(min(amount, 150), 0)
 
         await player.set_volume(amount)
-        await ctx.send(_("Changed volume to %s") % player.volume)
+        await ctx.send("Changed volume to %s" % player.volume)
 
     @commands.command(aliases=["stop"])
     @commands.guild_only()
     @commands.cooldown(1, 3, commands.BucketType.user)
     async def disconnect(self, ctx):
-        _ = await self._get_text(ctx)
         player = self.bot.lavalink.players.get(ctx.guild.id)
 
         if not player.is_connected:
-            return await ctx.send(_("I'm not connected to your vc ;w;"))
+            return await ctx.send("I'm not connected to your vc ;w;")
 
         if not ctx.author.voice or (player.is_connected and ctx.author.voice.channel.id != int(player.channel_id)):
-            return await ctx.send(_("You're not in my voice channel >_<"))
+            return await ctx.send("You're not in my voice channel >_<")
 
         player.queue.clear()
         await player.disconnect()
-        await ctx.send(_("baibai"))
+        await ctx.send("baibai")
 
     @commands.command(aliases=["playing"])
     @commands.guild_only()
     @commands.cooldown(1, 5, commands.BucketType.user)
     async def queue(self, ctx):
         """Check whats currently playing"""
-        _ = await self._get_text(ctx)
         player = self.bot.lavalink.players.get(ctx.guild.id)
 
         em = discord.Embed(color=0xDEADBF)
-        em.title = _("Currently Playing")
+        em.title = "Currently Playing"
 
         desc = ""
         if player.current:
@@ -295,10 +275,10 @@ class Audio:
                 break
             desc += "%s. **%s**\n" % (i, song.title.replace("@", "@\u200B"))
         if desc == "":
-            desc += "🔉" + _("Nothing...")
+            desc += "🔉 Nothing..."
 
         em.description = desc
-        em.set_footer(text=_("%s in queue") % (len(player.queue),))
+        em.set_footer(text="%s in queue" % (len(player.queue),))
         await ctx.send(embed=em)
 
     @commands.command()
@@ -306,16 +286,15 @@ class Audio:
     @commands.cooldown(1, 3, commands.BucketType.user)
     async def skip(self, ctx):
         """Skip a song"""
-        _ = await self._get_text(ctx)
         player = self.bot.lavalink.players.get(ctx.guild.id)
 
         if not player.is_playing:
-            return await ctx.send(_("I'm not playing anything"))
+            return await ctx.send("I'm not playing anything")
 
         if not ctx.author.voice or ctx.author.voice.channel.id != int(player.channel_id):
-            return await ctx.send(_("You're not in my voice channel >_<"))
+            return await ctx.send("You're not in my voice channel >_<")
 
-        await ctx.send(_("Skipped **%s**") % player.current.title.replace("@", "@\u200B"))
+        await ctx.send("Skipped **%s**" % player.current.title.replace("@", "@\u200B"))
         await player.skip()
 
     @commands.group(aliases=["playlists"])
@@ -417,7 +396,7 @@ class Audio:
     @playlist.command(name="play")
     async def playlist_play(self, ctx, playlist_name):
         """Add one of your playlists to the queue"""
-        _ = await self._get_text(ctx)
+
         playlists = (await r.table("playlists").get(str(ctx.author.id)).run(self.bot.r_conn))["playlists"]
         if not playlists.get(playlist_name):
             return await ctx.send("You don't have a playlist with that name >~<")
@@ -427,20 +406,20 @@ class Audio:
 
         if not player.is_connected:
             if not ctx.author.voice or not ctx.author.voice.channel:
-                return await ctx.send(_("You are not in any voice channel ;w;"))
+                return await ctx.send("You are not in any voice channel ;w;")
 
             permissions = ctx.author.voice.channel.permissions_for(ctx.me)
 
             if not permissions.connect or not permissions.speak:
-                return await ctx.send(_("Missing permissions to connect or speak ;w;"))
+                return await ctx.send("Missing permissions to connect or speak ;w;")
             player.store("channel", ctx.channel.id)
             await player.connect(ctx.author.voice.channel.id)
         else:
             if player.connected_channel.id != ctx.author.voice.channel.id:
-                return await ctx.send(_("Join my voice channel you baka"))
+                return await ctx.send("Join my voice channel you baka")
 
         if len(player.queue) + len(playlist) > 50:
-            return await ctx.send(_("You are too much songs in your queue you baka"))
+            return await ctx.send("You are too much songs in your queue you baka")
 
         for track in playlist:
             player.add(requester=ctx.author.id, track=track)
